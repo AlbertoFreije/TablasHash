a070f72fb99a42eff3f1f5488946fa83680cd109
AlbertoFreije <freicartobe@gmail.com>
2022-12-02 11:41:44 +0100

    Mejoras

--- TablasHash/src/p4Hash/ClosedHashTable.java
+++ TablasHash/src/p4Hash/ClosedHashTable.java
@@ -1,7 +1,5 @@
 package p4Hash;
 
-import java.lang.ProcessHandle.Info;
-
 /**
  * @author Profesores ED
  * @version 2022-23 distribuible
@@ -10,74 +8,76 @@ import java.lang.ProcessHandle.Info;
 public class ClosedHashTable<T> extends AbstractHash<T> {
 // IMPORTANTE
 //	No cambiar el nombre ni visibilidad de los atributos protected
-	
-	protected HashNode<T> tabla[]; //vecto tabla de tipo hashNode
 
-	protected int hashSize;	// tamaño de la tabla, debe ser un numero primo, el B
-	protected int numElems;	// numero de elementos en la tabla en cada momento.
+	protected HashNode<T> tabla[]; // vecto tabla de tipo hashNode
 
-	//Estos tres estados son por el borrado perezoso
-	public static final int LINEAL = 0;	// Tipo de exploracion en caso de colision, por defecto sera LINEAL
+	protected int hashSize; // tamaño de la tabla, debe ser un numero primo, el B
+	protected int numElems; // numero de elementos en la tabla en cada momento.
+
+	// Estos tres estados son por el borrado perezoso
+	public static final int LINEAL = 0; // Tipo de exploracion en caso de colision, por defecto sera LINEAL
 	public static final int CUADRATICA = 1;
 	public static final int DOBLEHASH = 2;
 
-	protected int tipoExploracion; //exploracion que se realizara en caso de colision (LINEAL por defecto)
-	
-	//REDISPERSION aumentar el tama�o de la tabla hash para evitar colisiones LF >=0.5
-	//REDISPERSION INVERSA disminuye el tama�o si el factor balance < 0.16 
+	protected int tipoExploracion; // exploracion que se realizara en caso de colision (LINEAL por defecto)
+
+	// REDISPERSION aumentar el tama�o de la tabla hash para evitar colisiones LF
+	// >=0.5
+	// REDISPERSION INVERSA disminuye el tama�o si el factor balance < 0.16
 
 	/**
-	 * Constructor para fijar el tamano al numero primo >= que el parametro y el tipo de exploración al indicado
-	 * el tipo de exploracion(LINEAL=0, CUADRATICA=1, ...), si invalido LINEAL
+	 * Constructor para fijar el tamano al numero primo >= que el parametro y el
+	 * tipo de exploración al indicado el tipo de exploracion(LINEAL=0,
+	 * CUADRATICA=1, ...), si invalido LINEAL
 	 */
 	@SuppressWarnings("unchecked")
 	public ClosedHashTable(int tam, int expl) {
-		if(!isPositivePrime(tam)) {
-			hashSize=nextPrimeNumber(tam);// Establece un tamaño valido si tam no es primo
-		}else {
-			hashSize=tam;
+		if (!isPositivePrime(tam)) {
+			hashSize = nextPrimeNumber(tam);// Establece un tamaño valido si tam no es primo
+		} else {
+			hashSize = tam;
 		}
-		
+
 		tabla = (HashNode<T>[]) new HashNode[hashSize]; // Crea el array de HashNode's
-		//			Completar lo que falta...
-		if(expl!=0) {
+		// Completar lo que falta...
+		if (expl != 0) {
 			this.tipoExploracion = expl;
-		}else {
-			this.tipoExploracion=LINEAL;
+		} else {
+			this.tipoExploracion = LINEAL;
 		}
-		this.numElems=0;
-		
-		for(int i=0; i<hashSize;i++) {
+		this.numElems = 0;
+
+		for (int i = 0; i < hashSize; i++) {
 			tabla[i] = new HashNode<>();
 		}
-		
 
 	}
 
 	/**
-	 * Constructor para fijar el tamaño al numero primo >= que el parametro
-	 * Se le pasa el tamaño de la table Hash, si no es un numero primo lo ajusta al primo superior
-	 * el factor de carga limite, por encima del cual hay que redispersar (directa)
-	 * el factor de carga limite, por debajo del cual hay que redispersar (inversa)
-	 * el tipo de exploracion(LINEAL=0, CUADRATICA=1, ...), si invalido LINEAL
+	 * Constructor para fijar el tamaño al numero primo >= que el parametro Se le
+	 * pasa el tamaño de la table Hash, si no es un numero primo lo ajusta al primo
+	 * superior el factor de carga limite, por encima del cual hay que redispersar
+	 * (directa) el factor de carga limite, por debajo del cual hay que redispersar
+	 * (inversa) el tipo de exploracion(LINEAL=0, CUADRATICA=1, ...), si invalido
+	 * LINEAL
 	 */
 	public ClosedHashTable(int tam, double fcUP, double fcDOWN, int expl) { // Para la segunda clase
-		//			Completar lo que falta...
+		// Completar lo que falta...
 
 	}
 
 	@Override
 	public int getNumOfElems() {
-		
+
 		return numElems;
-		
+
 	}
 
 	@Override
 	public int getSize() {
 		return hashSize;
 	}
-	
+
 	public int funcionDistribucion(T info, int intento) {
 
 		int posicion = fHash(info);
@@ -106,11 +106,10 @@ public class ClosedHashTable<T> extends AbstractHash<T> {
 
 	}
 
-	
-	//inserto elemento si encuentro vacio o borrado
-	//si encuentro lleno salto y realizo otro intento mas
+	// inserto elemento si encuentro vacio o borrado
+	// si encuentro lleno salto y realizo otro intento mas
 	// al insertar a la celda le pongo la info del elemento y lo pongo a lleno
-	//num elementos ++
+	// num elementos ++
 	@Override
 	public boolean add(T elem) {
 
@@ -120,87 +119,116 @@ public class ClosedHashTable<T> extends AbstractHash<T> {
 
 		int intento = 0;
 		int pos = funcionDistribucion(elem, intento);
-		
-		if(tabla[pos].getStatus()==1) {
-			return false;
-//			intento++;
-//			pos = funcionDistribucion(elem, intento);
-		}
-		
-		if(numElems < hashSize) {
-			tabla[pos].setInfo(elem);
-			
-			numElems++;
-			
-			return true;
+
+		while (intento <= hashSize) {
+
+			if (tabla[pos].getStatus() == 0 || tabla[pos].getStatus() == 2) {
+				if (numElems < hashSize) {
+					tabla[pos].setInfo(elem);
+
+					numElems++;
+
+					return true;
+				}
+			} else {
+				intento++;
+				pos = funcionDistribucion(elem, intento);
+			}
+
 		}
 
-	
 		return false;
-		
+
 	}
 
-	
-	//coge la posicion donde deberia estar
-	//el find pare cuando encuentra una casilla vacia
-	//el numerp de intentos sea igual al numero de elementos de la tabla hash para
-	//devuelve getInfo de la tabla hash
-	//sigue si encuentra un lleno que no es el o un borrado
+	// coge la posicion donde deberia estar
+	// el find pare cuando encuentra una casilla vacia
+	// el numerp de intentos sea igual al numero de elementos de la tabla hash para
+	// devuelve getInfo de la tabla hash
+	// sigue si encuentra un lleno que no es el o un borrado
 	//
 	@Override
 	public T find(T elem) {
-	
-		int iteracion =0;
-		int posicion = funcionDistribucion(elem,iteracion);
-		T prueba = tabla[posicion].getInfo();
-		while(!tabla[posicion].getInfo().equals(elem) || iteracion == numElems) {
-			
-			if(tabla[posicion].getStatus()==0) {
-				return null;
-				
+
+		int iteracion = 0;
+		int posicion = funcionDistribucion(elem, iteracion);
+		while ((tabla[posicion].getStatus() !=0 && !tabla[posicion].getInfo().equals(elem))) {
+
+			if (iteracion != numElems) {
+
+				if (tabla[posicion].getStatus() == 0) {
+					return null;
+
+				}
+				if (tabla[posicion].getInfo() != elem || tabla[posicion].getStatus() == 2) {
+
+					iteracion++;
+					posicion = funcionDistribucion(elem, iteracion);
+
+				}
+
 			}
-			if(tabla[posicion].getInfo() != elem || tabla[posicion].getStatus()==2) {
-				
-				iteracion++;
-				posicion = funcionDistribucion(elem,iteracion);
-				
-			}
-			
-			
+
 		}
-		
-		if(tabla[posicion].getInfo() == elem) {
+
+		if (tabla[posicion].getInfo() == elem) {
 			return tabla[posicion].getInfo();
 		}
-		
+
 		return null;
-		
+
 	}
 
 	@Override
 	public boolean remove(T elem) {
-	//			Completar lo que falta...
+		
+		if (elem == null) {
+			throw new NullPointerException();
+		}
+		
+		if(find(elem)== null) {
+			return false;
+		}
+		
+		int iteracion = 0;
+		
+		int pos = funcionDistribucion(elem, iteracion);
+		
+		while(tabla[pos].getStatus()==1 && tabla[pos].getInfo()!=elem) {
+			iteracion++;
+			pos =  funcionDistribucion(elem, iteracion);
+		}
+		
+		tabla[pos].remove();
+		numElems--;
+		
+		return true;
+	
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	protected boolean reDispersion () { // Para la segunda clase
-	//			Completar lo que falta...
+	protected boolean reDispersion() {
+		return false; // Para la segunda clase
+		// Completar lo que falta...
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	protected boolean inverseReDispersion() { // Para la segunda clase
-	//			Completar lo que falta...
+	protected boolean inverseReDispersion() {
+		return false; // Para la segunda clase
+		// Completar lo que falta...
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
-	public String toString (){
-		StringBuilder cadena=new StringBuilder();
-		for (int i=0;i< getSize();i++){
+	public String toString() {
+		StringBuilder cadena = new StringBuilder();
+		for (int i = 0; i < getSize(); i++) {
 			cadena.append(tabla[i]);
 			cadena.append(";");
 		}
@@ -212,5 +240,4 @@ public class ClosedHashTable<T> extends AbstractHash<T> {
 		return cadena.toString();
 	}
 
-	
 }
--- TablasHash/src/p4Hash/ClosedHashTableTest.java
+++ TablasHash/src/p4Hash/ClosedHashTableTest.java
@@ -9,15 +9,36 @@ class ClosedHashTableTest {
 	@Test
 	void addtest() {
 		
-		ClosedHashTable<Integer> tabla = new ClosedHashTable<>(5, 0);
-		assertEquals(true, tabla.add(1));
-		assertEquals(true, tabla.add(3));
-		assertEquals(true, tabla.add(4));
+		ClosedHashTable<Integer> tabla = new ClosedHashTable<>(13, 0);
 		assertEquals(true, tabla.add(5));
-		assertEquals(false, tabla.add(6));
+		assertEquals(true, tabla.add(8));
+		assertEquals(true, tabla.add(11));
+		assertEquals(true, tabla.add(9));
+		assertEquals(true, tabla.add(5));
+		assertEquals(true, tabla.add(7));
+		assertEquals(true, tabla.add(8));
+		assertEquals(true, tabla.add(6));
+		assertEquals(true, tabla.add(14));
+		System.out.println(tabla.toString());
 		
-		assertEquals(1, tabla.find(1));
+		
+		assertEquals(7, tabla.find(7));
+		assertEquals(null, tabla.find(45));
+		assertEquals(6, tabla.find(6));
 
 	}
+	
+	@Test
+	void deletetest() {
+		
+		ClosedHashTable<Integer> tabla = new ClosedHashTable<>(5, 0);
+		assertEquals(true, tabla.add(4));
+		assertEquals(true, tabla.add(13));
+		assertEquals(true, tabla.add(24));
+		assertEquals(true, tabla.add(3));
+		assertEquals(true, tabla.remove(24));
+		assertEquals(true, tabla.remove(3));
+		System.out.println(tabla.toString());
 
+	}
 }
